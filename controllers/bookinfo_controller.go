baaeba8adbe07fafccd518f838c0fb7310c5cb00
Ernesto González <ernesgonzalez.33.egc@icloud.com>
2023-03-01 11:47:24 +0100

    Add permissions for Service Mesh resources

--- controllers/bookinfo_controller.go
+++ controllers/bookinfo_controller.go
@@ -73,6 +73,8 @@ type BookinfoReconciler struct {
 //+kubebuilder:rbac:groups=deploy.kubernesto.io,resources=bookinfoes/finalizers,verbs=update
 //+kubebuilder:rbac:groups=core,resources=events,verbs=create;patch
 //+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=maistra.io,resources=servicemeshmembers,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups="networking.istio.io",resources=destinationrule;virtualservice;gateway,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch
 //+kubebuilder:rbac:groups=core,resources=services,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=core,resources=serviceaccounts,verbs=get;list;watch;create;update;patch;delete
